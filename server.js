d9c4a6fb111ed8f0c98438778ccba1bcdc94a1ba
AlfredBryan <alfred.chimereze@gmail.com>
2019-01-18 09:21:13 +0100

    update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 const logger = require("morgan");
 require("dotenv").config();
 
@@ -25,6 +26,16 @@ app.use(cors());
 app.use("/api", billRouter);
 
 const port = process.env.PORT || 4000;
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server now listening @port ${port}`);
